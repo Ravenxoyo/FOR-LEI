8875133988ebedfa37a4a37eb9c5f59dd5b89e57
Zen <156103519+Ravenxoyo@users.noreply.github.com>
2025-05-11 11:20:36 +0800

    Update main.js

--- js/main.js
+++ js/main.js
@@ -3,7 +3,7 @@ onload = () => {
   const c = setTimeout(() => {
     document.body.classList.remove("not-loaded");
 
-    const titles = ('FOR U LEI ').split('')
+    const titles = ('HAPPY MOTHERS DAY').split('')
     const titleElement = document.getElementById('title');
     let index = 0;
 
